47737cf7b4eb6119e3f184bafd4930792911a9ca
Maliy Igor <imaliy@maximagroup.com>
2015-04-25 12:58:36 +0300

    Runner component integration

--- Module.php
+++ Module.php
@@ -9,4 +9,22 @@ class Module extends \yii\base\Module
     const VERSION = '0.0.1';
     
     public $modelMap = [];
+    
+    public function init()
+    {
+        parent::init();
+        $this->components = [
+            'runner' => [
+                'class' => 'filsh\yii2\runner\RunnerComponent',
+                'runners' => [
+//                    'first',
+                    'second' => function(array $params) {
+                        echo 'inline runner with params: ' . json_encode($params);
+                    }
+                ]
+            ]
+        ];
+        
+var_dump($this->runner->run('second', ['var' => 'value']));exit;
+    }
 }
--- commands/ImportController.php
+++ commands/ImportController.php
@@ -2,7 +2,22 @@
 
 namespace filsh\geonames\commands;
 
+use Yii;
+
 class ImportController extends \yii\console\Controller
 {
-    
+    /**
+     * Import timezones from specified source.
+     *
+     * This command load and parse Maxmind timezones csv data file.
+     *
+     * @param string $sourceUrl the path of the destination source file.
+     * @throws Exception if the path argument is invalid.
+     */
+    public function actionTimezones($sourceUrl = self::TIMEZONES_SOURCE_URL)
+    {
+        Yii::$app->runner->run('timezones', [
+            'sourceUrl' => $sourceUrl
+        ]);
+    }
 }
